ae0db1cc0cfe65143127cf4e1a5768b019305a8f
chensoul <ichensoul@gmail.com>
2024-11-29 18:33:20 +0800

    part04

--- SpringBootOauth2Client/src/main/java/willydekeyser/config/SecurityConfig.java
+++ SpringBootOauth2Client/src/main/java/willydekeyser/config/SecurityConfig.java
@@ -1,24 +1,23 @@
 package willydekeyser.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-		.csrf().disable()
-		.cors().disable()
-			.authorizeHttpRequests(authorize -> authorize
-					.anyRequest().permitAll()) //.authenticated())
-			.oauth2Login(withDefaults())
-			.oauth2Client(withDefaults());
-		return http.build();
-	}
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()) //.authenticated())
+                .oauth2Login(withDefaults())
+                .oauth2Client(withDefaults());
+        return http.build();
+    }
 }
