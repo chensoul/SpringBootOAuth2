b106b929a4b00e39fb66e2a71cf1503df66713e8
chensoul <ichensoul@gmail.com>
2025-06-01 23:18:31 +0800

    feat(security): 更新 OAuth2配置
    - 在 SecurityConfig 中添加 OidcScopes 导包
    - 修改 RegisteredClient 配置，使用 OPENID 范围和新的重定向 URI
    - 移除未使用的 TokenSettings 和 ClientSettings Bean
    - 更新 Resource Server 端口为 8090

--- SpringBootOauth2AuthorizationServer/src/main/java/willydekeyser/config/SecurityConfig.java
+++ SpringBootOauth2AuthorizationServer/src/main/java/willydekeyser/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -77,10 +78,8 @@ public class SecurityConfig {
         RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
                 .clientId("client")
                 .clientSecret("secret")
-                .scope("read")
-                .redirectUri("https://oidcdebugger.com/debug")
-                .redirectUri("https://oauthdebugger.com/debug")
-                .redirectUri("https://springone.io/authorized")
+                .scope(OidcScopes.OPENID)
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/myoauth2")
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .build();
@@ -91,19 +90,6 @@ public class SecurityConfig {
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder().build();
-    }
-
-    @Bean
-    public TokenSettings tokenSettings() {
-        return TokenSettings.builder().build();
-    }
-
-    @Bean
-    public ClientSettings clientSettings() {
-        return ClientSettings.builder()
-                .requireAuthorizationConsent(false)
-                .requireProofKey(false)
-                .build();
     }
 
     @Bean
